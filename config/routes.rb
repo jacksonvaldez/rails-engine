3d9dab7f1c054eef99d7bd4320f41be162a26ec2
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-02-08 14:54:40 -0700

    Add routes to routes.rb to match the routes in the Postman tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      # RESTful routes
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index]
+      end
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        resource :merchant, only: [:show]
+      end
+
+      # Non-RESTful routes
+      get '/merchants/find_all', to: 'search#index'
+      get '/items/find', to: 'search#show'
+
+    end
+  end
+
+
+
 end
