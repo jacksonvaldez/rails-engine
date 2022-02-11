3b83d59438a52a3420e5650b77b23d9604f42997
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-02-10 21:18:07 -0700

    Refactor model tests to address changes from previous commit

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Invoice do
   end
 
   describe 'relationships' do
-    it { should have_many(:invoice_items) }
+    it { should have_many(:invoice_items).dependent(:delete_all) }
     it { should have_many(:items).through(:invoice_items) }
   end
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Item do
 
   describe 'relationships' do
     it { should belong_to(:merchant) }
-    it { should have_many(:invoice_items) }
+    it { should have_many(:invoice_items).dependent(:delete_all) }
     it { should have_many(:invoices).through(:invoice_items) }
   end
 
